4be5da03d5ec6cd16684b44cad004fcf9513498d
Mike Bowman <mbowman2@gmail.com>
2021-08-15 19:00:34 -0400

    Test dev

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,6 +4,7 @@ import Footer from './Footer';
 
 const Layout = ({ children }) => (
   <main className="bg-gray-300 max-w-screen-lg mt-12 mb-16 mx-auto">
+    DEV
     {children}
     <Footer />
   </main>
