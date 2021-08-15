108b497a2197739959b6b15ca112609784d266aa
Mike Bowman <mbowman2@gmail.com>
2021-08-15 18:36:19 -0400

    Tweak exports

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export const Footer = () => (
+const Footer = () => (
   <footer>
     <p className="text-center">
       Mike Bowman &copy; 2016 - {new Date().getFullYear()}
     </p>
   </footer>
 );
+export default Footer;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Footer } from './Footer';
+import Footer from './Footer';
 
 const Layout = ({ children }) => (
   <main className="bg-gray-300 max-w-screen-lg mt-12 mb-16 mx-auto">
