eed1670b60696c9245665ba1440e5c732b669511
Mike Bowman <mbowman2@gmail.com>
2021-08-17 19:17:18 -0400

    Start styling nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+const NavItem = ({ path, text }) => (
+  <li className="bg-green-200">
+    <Link to={path}>{text}</Link>
+  </li>
+);
 const Nav = () => (
   <nav className="bg-blue-200">
-    <ul>
-      <li>
-        <Link to="/">Mike Bowman</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-      <li>
-        <Link to="/blog">Blog</Link>
-      </li>
+    <ul className="flex justify-around">
+      <NavItem path="/" text="Mike Bowman" />
+      <NavItem path="/about" text="About" />
+      <NavItem path="/contact" text="Contact" />
+      <NavItem path="/blog" text="Blog" />
     </ul>
   </nav>
 );
