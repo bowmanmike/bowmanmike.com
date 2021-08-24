cd540aea401c949e6a37819cbb9e3edb8793875a
Mike Bowman <mbowman2@gmail.com>
2021-08-23 21:01:23 -0400

    Update some colours

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import Header from './Header';
 import Footer from './Footer';
 
 const Layout = ({ children }) => (
-  <main className="max-w-4xl mx-auto mt-5 px-4 pt-6 text-gray-800">
+  <main className="max-w-4xl mx-auto mt-5 px-4 pt-6 text-gray-600">
     <Header />
     {children}
     <Footer />
