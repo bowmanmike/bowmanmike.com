c7e3c02cdd7731e8b5b35e700b22de07114b4a19
Mike Bowman <mbowman2@gmail.com>
2021-08-23 21:11:28 -0400

    Add a bit of content to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,15 @@ import Seo from '../components/Seo';
 const IndexPage = () => (
   <>
     <Seo title="Home" />
-    <h2>Index</h2>
+    <h2>Hi! I'm Mike.</h2>
+    <p>I'm a software developer based in Toronto.</p>
+    <p>
+      Right now, I work at <a href="https://pagerduty.com">PagerDuty</a>
+    </p>
+    <p>
+      Previously, I've worked at <a href="https://thescore.com">theScore</a> and{' '}
+      <a href="https://zoocasa.com">Zoocasa.</a>
+    </p>
   </>
 );
 
