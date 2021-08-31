ec7d550ec30e08d210ba03f666c25ff93342bdfb
Mike Bowman <mbowman2@gmail.com>
2021-08-30 20:50:40 -0400

    Update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,13 +4,13 @@ import { StaticImage } from 'gatsby-plugin-image';
 import Seo from '../components/Seo';
 
 const AboutPage = () => (
-  <div className="">
+  <div>
     <Seo title="About" />
     <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
       <div className="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen" />
       <div className="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
         <div>
-          <h3 className="mt-2 text-2xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <h3 className="mt-2 text-xl leading-8 tracking-tight sm:text-2xl">
             Hi! I'm Mike!
           </h3>
         </div>
@@ -33,53 +33,42 @@ const AboutPage = () => (
         <div className="mt-8 lg:mt-0">
           <div className="text-base max-w-prose mx-auto lg:max-w-none">
             <p className="text-lg text-gray-500">
-              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
-              urna sed consectetur neque tristique pellentesque. Blandit amet,
-              sed aenean erat arcu morbi.
+              I'm a software developer based in Toronto. I love writing clean,
+              well-tested code, solving complex problems, and cold-brew coffee.
             </p>
           </div>
-          <div className="mt-5 prose prose-indigo text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
+          <div className="mt-5 prose mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
             <p>
-              Sollicitudin tristique eros erat odio sed vitae, consequat turpis
-              elementum. Lorem nibh vel, eget pretium arcu vitae. Eros eu
-              viverra donec ut volutpat donec laoreet quam urna.
+              Software Development brings me a fulfilling career that keeps me
+              energized, engaged, and excited. I love the feeling of seeing a
+              green test suite, especially after hours of wrangling with tricky
+              code. I take enormous satisfaction in looking back at things I've
+              built.
             </p>
             <p>
-              Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique sit
-              nisi lorem pulvinar. Urna, laoreet fusce nibh leo. Dictum et et et
-              sit. Faucibus sed non gravida lectus dignissim imperdiet a.
+              With an unconventional background, including a degree in Early
+              Modern Philosophy, I bring fresh ideas to the table. I'm always
+              open to trying something different, and can think outside the box
+              to find answers.
             </p>
             <p>
-              Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem
-              arcu, metus, egestas netus cursus. In.
+              I've had success in the software industry in many areas,
+              including:
             </p>
-            <ul role="list">
-              <li>Quis elit egestas venenatis mattis dignissim.</li>
+            <ul>
               <li>
-                Cras cras lobortis vitae vivamus ultricies facilisis tempus.
+                Architecting and developing back-end microservices using Elixir
+                and Phoenix
               </li>
-              <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+              <li>
+                Maintaining a significant legacy codebase in Ruby on Rails
+              </li>
+              <li>Building concurrent command-line tooling using Go</li>
             </ul>
-            <p>
-              Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt
-              vel ultricies. Vulputate aliquet velit faucibus semper.
-              Pellentesque in venenatis vestibulum consectetur nibh id. In id ut
-              tempus egestas. Enim sit aliquam nec, a. Morbi enim fermentum
-              lacus in. Viverra.
-            </p>
-            <h3>How we helped</h3>
-            <p>
-              Tincidunt integer commodo, cursus etiam aliquam neque, et.
-              Consectetur pretium in volutpat, diam. Montes, magna cursus nulla
-              feugiat dignissim id lobortis amet. Laoreet sem est phasellus eu
-              proin massa, lectus. Diam rutrum posuere donec ultricies non
-              morbi. Mi a platea auctor mi.
-            </p>
-            <p>
-              Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
-              urna sed consectetur neque tristique pellentesque. Blandit amet,
-              sed aenean erat arcu morbi.
-            </p>
+            {/*
+            <h3>How I Can Help You</h3>
+            <p>I can help in lots of ways</p>
+            */}
           </div>
         </div>
       </div>
