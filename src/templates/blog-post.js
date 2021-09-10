2e7b77b58013cd9416ac6830b87a446e76210fd2
Mike Bowman <mbowman2@gmail.com>
2021-09-09 21:31:21 -0400

    Minor styling for blog post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,9 +3,9 @@ import { graphql, Link } from 'gatsby';
 
 const NextPrev = ({ prevSlug, nextSlug }) => {
   return (
-    <div>
-      {prevSlug && <Link to={`/blog/${prevSlug}`}>Previous</Link>}
-      {nextSlug && <Link to={`/blog/${nextSlug}`}>Next</Link>}
+    <div className="flex justify-center">
+      {prevSlug && <Link to={`/blog/${prevSlug}`}>&#8592; Previous</Link>}
+      {nextSlug && <Link to={`/blog/${nextSlug}`}>Next &#8594;</Link>}
     </div>
   );
 };
@@ -15,9 +15,12 @@ const BlogPost = ({ data: { post }, pageContext: { prevSlug, nextSlug } }) => {
     <>
       <NextPrev nextSlug={nextSlug} prevSlug={prevSlug} />
       <article>
-        <h2>{post.frontmatter.title}</h2>
-        <small>{post.frontmatter.date}</small>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <h2 className="text-xl my-1">{post.frontmatter.title}</h2>
+        <small className="text-lg">{post.frontmatter.date}</small>
+        <div
+          dangerouslySetInnerHTML={{ __html: post.html }}
+          className="prose"
+        />
       </article>
       <NextPrev nextSlug={nextSlug} prevSlug={prevSlug} />
     </>
