f40193674cc5c9096b657752eb8deff3bc548133
Mike Bowman <mbowman2@gmail.com>
2021-11-20 17:13:11 -0500

    Tweak contact stuff

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,37 @@
 import React from 'react';
+import { FaGithub } from '@react-icons/all-files/fa/FaGithub';
+import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin';
+import { FaEnvelopeOpen } from '@react-icons/all-files/fa/FaEnvelopeOpen';
+
+const ContactMethod = ({ href, text, children }) => (
+  <li className="my-2">
+    <a href={href}>
+      <p className="border border-gray-400 shadow-sm rounded-md p-2 inline-block hover:bg-gray-100 w-1/2 text-center">
+        {children}
+        {text}
+      </p>
+    </a>
+  </li>
+);
 
 const Contact = () => (
-  <div className="bg-orange-200 grid grid-cols-1">
-    <div className="text-center text-2xl h-auto flex items-end pb-4">
-      <p>Find me at...</p>
-    </div>
-    <ul className="h-auto text-lg">
-      <li className="">
-        <p>
-          <a href="mailto:mike@mikebowman.dev">mike@mikebowman.dev</a> by Email
-        </p>
-      </li>
-      <li className="">
-        <p className="">
-          <a href="https://github.com/bowmanmike">bowmanmike</a> on Github
-        </p>
-      </li>
-      <li className="">
-        <p className="">
-          <a href="https://www.linkedin.com/in/bowmanmike1/">mikebowman</a> on
-          Linkedin
-        </p>
-      </li>
+  <div className="bg-orange-200 grid items-center">
+    <ul className="text-lg text-center">
+      <ContactMethod
+        text="mike@mikebowman.dev"
+        href="mailto:mike@mikebowman.dev"
+      >
+        <FaEnvelopeOpen className="inline-block mr-2" />
+      </ContactMethod>
+      <ContactMethod text="bowmanmike" href="https://github.com/bowmanmike">
+        <FaGithub className="inline-block mr-2" />
+      </ContactMethod>
+      <ContactMethod
+        href="https://www.linkedin.com/in/bowmanmike1/"
+        text="Mike Bowman"
+      >
+        <FaLinkedin className="inline-block mr-2" />
+      </ContactMethod>
     </ul>
   </div>
 );
