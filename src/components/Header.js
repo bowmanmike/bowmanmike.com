5cdd2f4e8425a54dbc9e3b86f606923d7a09420f
Mike Bowman <mbowman2@gmail.com>
2021-11-21 20:01:19 -0500

    Fix header responsive issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,7 @@
 import React from 'react';
-// import { Link } from 'gatsby';
-
-// import Nav from './Nav';
 
 const Header = () => (
-  <header className="max-w-full grid grid-cols-2">
+  <header className="max-w-full">
     <div className="p-8 ml-8">
       <p className="text-3xl font-semibold mb-2">Mike Bowman</p>
       <p className="font-mono text-xl italic">
