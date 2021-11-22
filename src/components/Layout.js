2fc49f3a99a6f2c873a687e096e09a4aa4253ff5
Mike Bowman <mbowman2@gmail.com>
2021-11-22 14:50:41 -0500

    Don't use google fonts

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import Helmet from 'react-helmet';
 
 import Header from './Header';
 import Footer from './Footer';
 
 const Layout = ({ children }) => (
   <>
-    <Helmet>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,600;1,300;1,400&display=swap"
-        rel="stylesheet"
-      />
-    </Helmet>
     <div className="max-w-6xl mx-auto text-gray-700">
       <Header />
       <main className="max-w-6xl mx-auto my-4">{children}</main>
