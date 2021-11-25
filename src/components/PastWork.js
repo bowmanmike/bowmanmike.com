280bfd85923d11f34c0788dc96561c7399aea0a0
Mike Bowman <mbowman2@gmail.com>
2021-11-25 17:21:06 -0500

    Trying to fix mobile

--- src/components/PastWork.js
+++ src/components/PastWork.js
@@ -9,7 +9,7 @@ import PagerDutyLogo from '../assets/images/pagerduty-logo.svg';
 const LogoCard = ({ href, baseColour, children }) => {
   const styles = { backgroundColor: baseColour };
   return (
-    <a href={href} className="border border-gray-400" style={styles}>
+    <a href={href} className="max-w-max p-2" style={styles}>
       <div className="h-full">{children}</div>
     </a>
   );
@@ -26,25 +26,28 @@ const PastWork = () => (
         <LHLLogo className="" width={250} height={50} />
       </a>
     </div>
-    <div className="md:grid-cols-4 grid items-center justify-center gap-4">
-      <h3 className="inline-block">Previously learned at...</h3>{' '}
+    <div className="grid md:grid-cols-4 items-center justify-items-center gap-4 max-w-4xl mb-4 text-center md:text-left">
+      <h3 className="inline-block col-span-2 md:col-span-1 justify-self-start">
+        Previously learned at...
+      </h3>{' '}
       <LogoCard href="https://pagerduty.com">
-        <PagerDutyLogo />
+        <PagerDutyLogo height="2.5rem" />
       </LogoCard>
       <LogoCard href="https://thescore.com" baseColour="#1a1a1c">
-        <TheScoreLogo />
+        <TheScoreLogo height="2.5rem" />
       </LogoCard>
       <LogoCard href="https://zoocasa.com" baseColour="#171a1c">
         <StaticImage
           src="../assets/images/zoocasa-logo.png"
           alt="Zoocasa Logo"
+          height="2.5rem"
         />
       </LogoCard>
     </div>
-    <div className="grid">
-      <p>Next Adventure: You!</p>
+    <div className="text-center md:text-left">
+      <p className="inline-block">Next Adventure: You!</p>
       <a href="mailto:mike@mikebowman.dev" className="inline-block">
-        <p className="border border-gray-400 shadow-lg p-2 inline-block bg-sage w-full md:w-1/2 text-center">
+        <p className="border border-gray-400 shadow-lg py-1 px-2 text-lg inline-block bg-sage w-full text-center mx-4">
           <FaEnvelopeOpen className="inline-block mr-2" />
           mike@mikebowman.dev
         </p>
