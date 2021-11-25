8310cce528230877be501abf2deb493662451c01
Mike Bowman <mbowman2@gmail.com>
2021-11-25 17:26:08 -0500

    Disable all old pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,45 +1,45 @@
-const path = require('path');
+// const path = require('path');
 
-const gql = String.raw;
+// const gql = String.raw;
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage, createRedirect } = actions;
-  const result = await graphql(gql`
-    query {
-      blog: allMarkdownRemark(
-        filter: { frontmatter: { published: { eq: true } } }
-        sort: { fields: frontmatter___date, order: DESC }
-      ) {
-        posts: edges {
-          node {
-            frontmatter {
-              slug
-              title
-            }
-          }
-        }
-      }
-    }
-  `);
+  const { createRedirect } = actions;
+  // const result = await graphql(gql`
+  //   query {
+  //     blog: allMarkdownRemark(
+  //       filter: { frontmatter: { published: { eq: true } } }
+  //       sort: { fields: frontmatter___date, order: DESC }
+  //     ) {
+  //       posts: edges {
+  //         node {
+  //           frontmatter {
+  //             slug
+  //             title
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `);
 
-  const { posts } = result.data.blog;
-  posts.forEach(({ node }, i) => {
-    const {
-      frontmatter: { slug },
-    } = node;
-    const nextPost = posts[i - 1]?.node?.frontmatter;
-    const prevPost = posts[i + 1]?.node?.frontmatter;
+  // const { posts } = result.data.blog;
+  // posts.forEach(({ node }, i) => {
+  //   const {
+  //     frontmatter: { slug },
+  //   } = node;
+  //   const nextPost = posts[i - 1]?.node?.frontmatter;
+  //   const prevPost = posts[i + 1]?.node?.frontmatter;
 
-    createPage({
-      path: `blog/${slug}`,
-      component: path.resolve('./src/templates/blog-post.js'),
-      context: {
-        slug,
-        prevPost,
-        nextPost,
-      },
-    });
-  });
+  //   createPage({
+  //     path: `blog/${slug}`,
+  //     component: path.resolve('./src/templates/blog-post.js'),
+  //     context: {
+  //       slug,
+  //       prevPost,
+  //       nextPost,
+  //     },
+  //   });
+  // });
 
   createRedirect({
     fromPath: '/js/script.js',
