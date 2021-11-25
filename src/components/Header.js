0b6dcf4c49a53f4781651fbd9a9f080080f11283
Mike Bowman <mbowman2@gmail.com>
2021-11-25 17:30:03 -0500

    Add link to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const Header = () => (
   <header className="mt-4">
     <div className="p-4 lg:px-0">
-      <p className="text-3xl font-semibold mb-2">Mike Bowman</p>
-      <p className="font-mono text-xl italic">
-        Freelance Full-Stack Software Developer
-      </p>
+      <Link to="/">
+        <p className="text-3xl font-semibold mb-2">Mike Bowman</p>
+        <p className="font-mono text-xl italic">
+          Freelance Full-Stack Software Developer
+        </p>
+      </Link>
     </div>
   </header>
 );
