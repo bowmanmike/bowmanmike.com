e5ffbd307198cc7a5130d3ddea34f9170b476808
Mike Bowman <mbowman2@gmail.com>
2021-12-07 16:31:14 -0500

    Start styling the blog index page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,15 +5,23 @@ import { Link, graphql } from 'gatsby';
 const Blog = ({ data }) => {
   const { posts } = data.blog;
   return (
-    <div>
+    <div className="p-2 lg:p-4 m-4 lg:m-0 shadow-md border border-gray-400">
+      <h2 className="text-2xl text-center mb-4 border-sage border-b-4 max-w-max pr-8">
+        All Posts
+      </h2>
       {posts.map(post => {
         const { slug } = post.frontmatter;
         return (
-          <div key={slug}>
-            <Link to={slug}>
-              {post.frontmatter.date} - {post.frontmatter.title}
-            </Link>
-          </div>
+          <Link key={slug} to={slug}>
+            <div className="p-2 lg:p-4">
+              <div className=" border-b-2 border-sage max-w-max pr-8">
+                <p className="text-lg">{post.frontmatter.title}</p>
+                <p className="italic">Published {post.frontmatter.date}</p>
+              </div>
+              <p>{post.excerpt}</p>
+              <p className="text-sm">Continue Reading...</p>
+            </div>
+          </Link>
         );
       })}
     </div>
@@ -32,6 +40,7 @@ export const pageQuery = graphql`
           slug
           date(formatString: "YYYY-MM-DD")
         }
+        excerpt
       }
     }
   }
