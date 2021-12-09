524e771ad0d12d887caa90d80022f55d40c4ea2c
Mike Bowman <mbowman2@gmail.com>
2021-12-09 15:21:35 -0500

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,15 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        ...colors,
-        sky: colors.sky,
         sage: { DEFAULT: '#a8b5a4' },
       },
       fontFamily: {
         sans: ['Nunito', 'sans-serif'],
       },
-    },
-  },
-  variants: {
-    extend: {
-      backgroundColor: ['active'],
-      margin: ['first'],
     },
   },
   plugins: [
