d8b2ae41c14cbeeaf205ea0167f4c8777c7b5e60
Mike Bowman <mbowman2@gmail.com>
2021-12-09 17:29:04 -0500

    Fix gatsby complaining

--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -3,13 +3,21 @@ import { Link } from 'gatsby';
 
 export default function NavButton({ href, content, icon, postIcon }) {
   const isDisabled = href === 'disabled';
+  if (isDisabled)
+    return (
+      <button
+        type="button"
+        disabled
+        className="pointer-events-none border border-gray-400 px-4 py-2 mb-4 inline-block hover:bg-sage-100 shadow-md"
+      >
+        {content}
+      </button>
+    );
+
   return (
     <Link
-      to={!isDisabled ? href : ''}
-      disabled={isDisabled}
-      className={`${
-        isDisabled ? 'pointer-events-none' : ''
-      } border border-gray-400 px-4 py-2 mb-4 inline-block hover:bg-sage-100 shadow-md`}
+      to={href}
+      className="border border-gray-400 px-4 py-2 mb-4 inline-block hover:bg-sage-100 shadow-md"
     >
       <p className="flex items-center justify-center">
         {icon}
