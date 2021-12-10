413a003a58cff1a4c82d689f18377897ef9c7fe9
Mike Bowman <mbowman2@gmail.com>
2021-12-10 14:40:23 -0500

    Add shadow to pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -14,7 +14,7 @@ export default function Pagination({
   const hasPrevPage = prevPage >= 1;
 
   return (
-    <div className="grid grid-flow-col md:w-2/3 md:mx-auto">
+    <div className="grid grid-flow-col md:w-2/3 md:mx-auto shadow-md">
       <Link
         className={`${
           !hasPrevPage ? 'pointer-events-none bg-gray-300' : ''
