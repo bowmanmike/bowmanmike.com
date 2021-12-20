f87eca01dd80061c5b1db021f795a4de93e5c688
Mike Bowman <mbowman2@gmail.com>
2021-12-20 15:43:41 -0500

    Desktop styles

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -42,7 +42,7 @@ const ContactForm = () => {
   };
 
   return (
-    <div className="border border-gray-400 p-2 m-4 shadow-md">
+    <div className="border border-gray-400 p-2 lg:p-4 m-4 lg:m-0 shadow-md">
       <h3 className="text-2xl mb-2 border-sage border-b-4 max-w-max pr-8">
         Get In Touch!
       </h3>
@@ -52,14 +52,13 @@ const ContactForm = () => {
         netlify-honeypot="cool-stuff"
         data-netlify="true"
         onSubmit={submitForm}
-        className="w-full"
+        className="w-full grid grid-cols-1 md:grid-cols-2 md:gap-2"
       >
         <input type="hidden" name="contact" value="contact" />
         <input type="hidden" name="cool-stuff" />
         {['name', 'email'].map(tag => (
           <label
             htmlFor={tag}
-            // className="w-full flex my-2 items-center justify-between"
             className="w-full block"
             key={`form-input-${tag}`}
           >
@@ -73,7 +72,7 @@ const ContactForm = () => {
               value={values[tag]}
               onChange={updateValues}
               required
-              className="block w-full my-2 outline-none"
+              className="block w-full my-2 outline-none shadow-md"
               placeholder={`Your ${startCase(tag)}`}
             />
           </label>
@@ -85,14 +84,19 @@ const ContactForm = () => {
             name="content"
             value={values.content}
             onChange={updateValues}
-            className="w-full my-2"
+            className="w-full my-2 shadow-md"
+            placeholder="Let me know what I can do for you!"
           />
         </label>
         <label>
           <span className="border-b-2 border-sage pr-2">
             How did you hear about me?
           </span>
-          <select name="from" onChange={updateValues} className="w-full my-2">
+          <select
+            name="from"
+            onChange={updateValues}
+            className="w-full my-2 shadow-md"
+          >
             {['word-of-mouth', 'social-media', 'google', 'none'].map(source => (
               <option value={source} key={`form-select-${source}`}>
                 {source === 'none'
@@ -104,7 +108,7 @@ const ContactForm = () => {
         </label>
         <button
           type="submit"
-          className="block w-full border border-gray-400 bg-sage-400 shadow-md mt-2"
+          className="block w-full border border-gray-400 bg-sage-400 shadow-md mt-2 md:col-span-2 md:w-1/4 mx-auto py-2"
         >
           Submit
         </button>
